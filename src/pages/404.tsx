1aa439a634d56857fa31cf5e975fe86a97de0b71
gyoungsubLee <dltjq2323@gmail.com>
2025-05-01 19:45:34 +0900

    feat: 2-1,2,3
    - 2.1 페이지 라팅
    - 2.2 네비이팅
    - 2.3 프리페칭

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,7 @@
 export default function Page() {
   return (
     <div>
-      <h1>Test Page</h1>
-      <p>This is a test page.</p>
+      <h1>404 - Page Not Found</h1>
     </div>
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,33 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  const onClinckBnt = () => {
+    router.push("/books/2");
+  };
+
+  useEffect(() => {
+    router.prefetch("/books/2");
+  }, []);
+
+  return (
+    <>
+      <header>
+        <Link href={"/"}>index</Link>
+        <Link href={"/search"} prefetch={false}>
+          search
+        </Link>
+        <Link href={"/books/1"}>books/1</Link>
+        <div>
+          <button onClick={onClinckBnt}>/books/2 페이지 이동</button>
+        </div>
+      </header>
+      <Component {...pageProps} />
+    </>
+  );
 }
